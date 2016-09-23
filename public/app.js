d961e64ec037b5cbc56fb1d8263a78d167098506
Pamela Argentino <argentinopam@hotmail.com>
2016-09-23 16:21:53 -0300

    Add messaging

--- public/app.js
+++ public/app.js
@@ -1,10 +1,13 @@
 var app = angular.module("angularFireApp", ["firebase"]);
 
-app.controller("SampleCtrl", function($scope, $firebaseObject) {
-  var ref = firebase.database().ref().child("data");
+app.controller("SampleCtrl", function($scope, $firebaseObject, $firebaseArray) {
+  var refMsg = firebase.database().ref().child("messages");
   
-  var syncObject = $firebaseObject(ref);
+  $scope.messages = $firebaseArray(refMsg);
   
-  syncObject.$bindTo($scope, "data");
-  
+  $scope.addMessage = function() {
+    $scope.messages.$add({
+      text: $scope.newMessageText
+    });
+  }
 });
